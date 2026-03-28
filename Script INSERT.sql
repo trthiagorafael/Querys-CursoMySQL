d0e4c4aeefbb97f6c1d10c8e491df4deeb26a39f
trthiagorafael <trthiagorafael@gmail.com>
2026-03-28 15:05:04 -0300

    Alterando o script de Inserts

--- Script INSERT.sql
+++ Script INSERT.sql
@@ -3,14 +3,14 @@ USE db_locadora;
 -- Detalhamento do Script
 
 -- Insert único
--- INSERT INTO tb_categoria (descricao) 
--- VALUES 
--- ('Jogos');
--- 
--- INSERT INTO tb_categoria (descricao) 
--- VALUES 
--- ('RPG');
--- 
+INSERT INTO tb_categoria (descricao) 
+VALUES 
+('Jogos');
+
+INSERT INTO tb_categoria (descricao) 
+VALUES 
+('RPG');
+
 
 -- Insert Aninhado
 INSERT INTO tb_categoria (descricao) VALUES ('RPG 1'),('RPG 2'),('RPG 3');
