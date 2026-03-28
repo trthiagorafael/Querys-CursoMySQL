f08715ecfa6a6cad32f8bf7d6bb5b204445f57f4
trthiagorafael <trthiagorafael@gmail.com>
2026-03-28 14:34:36 -0300

    Primeiro commit

---
+++ Script INSERT.sql
@@ -0,0 +1,16 @@
+USE db_locadora;
+
+-- Insert único
+-- INSERT INTO tb_categoria (descricao) 
+-- VALUES 
+-- ('Jogos');
+-- 
+-- INSERT INTO tb_categoria (descricao) 
+-- VALUES 
+-- ('RPG');
+-- 
+
+-- Insert Aninhado
+INSERT INTO tb_categoria (descricao) VALUES ('RPG 1'),('RPG 2'),('RPG 3');
+
+
