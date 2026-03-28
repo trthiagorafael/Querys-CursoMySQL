30173ee6d6872cb7d427cc40bf96ea4cba265dca
trthiagorafael <trthiagorafael@gmail.com>
2026-03-28 15:11:56 -0300

    Script Insert aninhado

---
+++ Script Insert Aninhado.sql
@@ -0,0 +1,11 @@
+USE db_locadora;
+
+-- Inert aninhado
+
+INSERT INTO tb_clientes (nome,telefone,email) VALUES
+('João','111111','email1@gmail.com'),
+('João','22222','email2@gmail.com'),
+('João','333333','email@gmail.com'),
+('João','4444','email3@gmail.com'),
+('João','555','email4@gmail.com'),
+('João','666','email5@gmail.com');
